1e0fe493d960d766d309e5b24107d9511b005721
Sean Mack <sjm3758@rit.edu>
2018-01-25 22:49:33 -0500

    Objects with the ChangeColor script can now change colors between red green and blue when the space key is pressed

--- Assets/Scripts/ChangeColor.cs
+++ Assets/Scripts/ChangeColor.cs
@@ -4,16 +4,54 @@ using UnityEngine;
 
 public class ChangeColor : MonoBehaviour {
 
+    //initializes the colors and temp variable for the given gameObjects renderer
+    public MeshRenderer gameobjectRenderer;
+    public Color red;
+    public Color green;
+    public Color blue;
+    private Color[] colors;
+    private int counter;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        counter = 0;
+        gameobjectRenderer = this.gameObject.GetComponent<MeshRenderer>();
+        gameobjectRenderer.material = this.gameObject.GetComponent<Material>();
+        //RGB colors set
+        red = new Color(255f, 0, 0, 1f);
+        blue = new Color(0, 0, 255f, 1f);
+        green = new Color(0, 255f, 0, 1f);
+        colors = new Color[] { red, green, blue };
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.C))
-        {
+        CycleColor();
+        this.gameObject.GetComponent<MeshRenderer>().material = gameobjectRenderer.material;
+    }
 
+    public void CycleColor()
+    {
+        /*if (Input.GetKeyDown(KeyCode.R))
+        {
+            gameobjectRenderer.material.color = red;
         }
-	}
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            gameobjectRenderer.material.color = blue;
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            gameobjectRenderer.material.color = green;
+        }*/
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            gameobjectRenderer.material.color = colors[counter];
+            counter++;
+        }
+        if (counter >= colors.Length)
+        {
+            counter = 0;
+        }
+    }
 }
