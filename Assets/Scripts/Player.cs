3d2429dc66d53227b4611789e062469a4c513e66
Sean Mack <sjm3758@rit.edu>
2018-02-02 01:54:56 -0500

    Added switch prefab. Player class modified: player now moves left and right with A and D keys, player changes color to that of a switch when colliding with one. Added basic physics to player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,25 +4,43 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    public float speed;
+
 	// Use this for initialization
 	void Start () {
-		
+        speed = 5.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        Move();
 	}
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "switch")
+        //Debug.Log("switch");
+        if (collision.gameObject.tag == "Switch")
         {
-            //change player to red if switch is red
-            
-            //change player to blue if switch is blue
-
-            //change player to green if switch is green
+            this.gameObject.GetComponent<MeshRenderer>().material = collision.gameObject.GetComponent<MeshRenderer>().material;
+            Debug.Log("switch hit!");
         }
     }
+
+    void Move()
+    {
+        Vector3 pos = this.gameObject.GetComponent<Transform>().position;
+        //move right
+        if (Input.GetKey(KeyCode.D))
+        {
+            pos.x += speed * Time.deltaTime;
+        }
+        //move left
+        if (Input.GetKey(KeyCode.A))
+        {
+            pos.x -= speed * Time.deltaTime;
+        }
+
+        this.gameObject.GetComponent<Transform>().position = pos;
+    }
+
 }
