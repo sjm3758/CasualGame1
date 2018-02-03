1be1a73d15df890c2a84dc9b4766e32475bf72cc
PlatFleece <maverick.alpha4@gmail.com>
2018-02-03 16:51:03 -0500

    Add files via upload

---
+++ Assets/ColorWheelRotate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorWheelRotate : MonoBehaviour {
+
+    private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("Change Color");
+        }
+        else
+        {
+
+        }
+
+	}
+}
