8507c2b6f5b6b504694213649552caf87269cb7b
Plat <maverick.alpha4@gmail.com>
2018-02-16 07:31:08 -0500

    Files Added

---
+++ Assets/Scripts/ButtonLoadScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonLoadScene : MonoBehaviour {
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(0);
+        
+    }
+}
+
+
---
+++ Assets/Scripts/ButtonLoadScene2.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonLoadScene2 : MonoBehaviour {
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(2);
+        //test
+    }
+}
