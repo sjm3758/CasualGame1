aa69f407964327013b6098fae2c5c64bb28d76d0
Plat <maverick.alpha4@gmail.com>
2018-02-16 08:15:38 -0500

    Quit Button Implemented

---
+++ Assets/Scripts/ButtonQuit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonQuit : MonoBehaviour {
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        //test
+    }
+}
